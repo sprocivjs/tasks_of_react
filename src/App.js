aa57fe4d2b72679e93f07e89eb2914372b544234
sprocivjs <sprocivjs@gmail.com>
2022-09-15 16:41:49 +0300

    counter updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.scss';
+import {useState} from "react";
+
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const onClickPlus = () => setCount(count + 1);
+  const onClickMinus = () => setCount(count - 1);
+  const onClickReset = () =>setCount(0);
+  const onClickRandom = () =>setCount(Math.floor(Math.random()*100));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div>
+          <h2>Counter on the React:</h2>
+          <h1>{count}</h1>
+          <button onClick={onClickMinus} className="minus">- Minus</button>
+          <button onClick={onClickReset} className="reset">Reset</button>
+          <button onClick={onClickRandom} className="random">Random</button>
+          <button onClick={onClickPlus} className="plus">Plus +</button>
+        </div>
+      </div>
   );
 }
 
